f2b1b22799533924224a1ca1b4b9a8980830f0f0
agusputra <agus.putra99@gmail.com>
2018-12-16 17:01:30 +0700

    impl all features

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'lodash'
 import axios from 'axios'
 import Product from './Product'
 
@@ -6,27 +7,146 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = { products: [] }
+    this.state = {
+      limit: 100,
+      products: [],
+      cache: [],
+      sort: null,
+      page: 0,
+      loading: false,
+      end: false,
+    }
   }
 
   componentDidMount() {
+
+    // Creates a throttled function that only invokes func at most once per every wait milliseconds.
+    document.addEventListener('scroll', _.throttle(e => {
+
+      // If scroll is 480px above bottom then load.
+      if (window.innerHeight + window.scrollY >= (document.body.scrollHeight - 480)) {
+        !this.state.loading && this.load(this.state.page)
+      }
+    }, 200))
+
+    this.load(1)
+  }
+
+  load(page, sort) {
+    if (this.state.loading || (this.state.end && !sort)) {
+      return
+    }
+
+    if (page === 1 || sort) {
+      this.loadFromServer(page, sort, products => {
+        this.setState({ products: products, cache: [] })
+
+        //Pre-emptively fetch the next results.
+        this.populateCache(page)
+      })
+    }
+    else if (this.state.cache.length) {
+      this.setState({ products: this.state.products.concat(this.state.cache), cache: [] })
+
+      //Pre-emptively fetch the next results.
+      this.populateCache(page)
+    }
+  }
+
+  populateCache(page) {
+    this.loadFromServer(page + 1, null, products => this.setState({ cache: products }))
+  }
+
+  loadFromServer(page, sort, cb) {
+    let query = `_limit=${this.state.limit}&_page=${page}`
+
+    if (sort) {
+      query += `&_sort=${sort}`
+    }
+    else if (this.state.sort) {
+      query += `&_sort=${this.state.sort}`
+    }
+
+    const state = { loading: true, page }
+
+    // If new sort then first we empty the products.
+    if (sort) {
+      state.sort = sort
+      state.end = false
+      state.products = []
+    }
+
+    this.setState(state)
+
     const l = document.location
-    axios.get(`${l.protocol}//${l.hostname}:3000/products?_page=1&_limit=15`)
+    console.log('LOAD: ', `${l.protocol}//${l.hostname}:3000/products?${query}`)
+    axios.get(`${l.protocol}//${l.hostname}:3000/products?${query}`)
       .then(res => {
-        this.setState({ products: res.data })
+        this.setState({ loading: false, end: res.data.length === 0 })
+
+        return cb && cb(insertAds(res.data))
       })
   }
 
   render() {
+    const sort = this.state.sort
+    const sortButtonClass = "btn btn-secondary ml-3"
+    const loading = this.state.loading
+
     return (
       <React.Fragment>
-        <div className="jumbotron"><h1>ASCII Faces Store</h1></div>
+        <a href="/"><div className="jumbotron"><h1>ASCII Faces Store</h1></div></a>
         <div className="container">
+          <div className="row">
+            <div className="col-12 text-right my-3">
+              <span>Sort:</span>
+              <button className={sortButtonClass} disabled={sort === 'size' || loading} onClick={() => this.load(1, 'size')}>Size</button>
+              <button className={sortButtonClass} disabled={sort === 'price' || loading} onClick={() => this.load(1, 'price')}>Price</button>
+              <button className={sortButtonClass} disabled={sort === 'id' || loading} onClick={() => this.load(1, 'id')}>ID</button>
+            </div>
+          </div>
           <div className="row">
             {this.state.products.map(p => (<Product product={p} key={p.id} />))}
           </div>
+          {
+            !!this.state.products.length &&
+            <p>Loaded {this.state.products.length} products</p>
+          }
+          {
+            loading &&
+            <div className="loading-indicator">
+              <p>..loading..</p>
+            </div>
+          }
+          {
+            this.state.end && <h2 className="my-5 text-center">~ end of catalogue ~</h2>
+          }
+          <button className="btn btn-top bg-dark text-muted" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>TOP</button>
         </div>
-      </React.Fragment>
+      </React.Fragment >
     )
+  }
+}
+
+function insertAds(products) {
+  const max = products.length
+  let i = 0
+  let step = 20
+
+  for (let j = step; j <= max; j += step) {
+    const id = generateUniqRandom()
+    products.splice(j + i++, 0, { id, type: 'ads', url: `http://localhost:3000/ads/?r=${id}` });
+  }
+
+  return products
+}
+
+const random = []
+function generateUniqRandom() {
+  const ran = Math.random()
+  if (_.find(random, ran)) {
+    return generateUniqRandom()
+  } else {
+    return ran
   }
 }
--- src/Product.js
+++ src/Product.js
@@ -1,25 +1,54 @@
 import React from 'react'
 import U from './Utils'
 
-export default function Product(props) {
-  const { product } = props
-  const date = U.formatDate(U.parseDateAndTz(product.date))
-
+function Ads({ product }) {
   return (
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card">
-        <div className="card-image-top text-center bg-dark text-light py-5" style={{ fontSize: product.size }}>
-          {product.face}
+    <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div className="card text-white bg-primary">
+        <div className="card-image-top">
+          <img className="w-100" src={product.url} alt="ads" />
         </div>
         <div className="card-body">
-          <h5 className="card-title">Face {product.id}</h5>
-          <p className="card-text">{date}</p>
-          <strong>${product.price / 100}</strong>
-        </div>
-        <div className="card-footer">
-          <button className="btn btn-secondary btn-block" onClick={() => alert("not implemented")}>Buy</button>
+          <div className="card-text">
+            <strong className="card-title">Ads</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, minima.</p>
+          </div>
         </div>
       </div>
     </div>
   )
+}
+
+function ProductCard({ product }) {
+  const date = U.formatDate(U.parseDateAndTz(product.date))
+
+  return (
+    <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div className="card">
+        <div className="card-image-top text-center text-light py-5 bg-dark" style={{ fontSize: product.size }}>
+          {product.face}
+        </div>
+        <div className="card-body">
+          <strong className="card-title">ID: {product.id}</strong>
+          <div className="card-text">
+            <div>{date}</div>
+            <div>{product.size} px</div>
+          </div>
+          <strong>${product.price / 100}</strong>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-secondary btn-block">Buy</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default function Product({ product }) {
+  if (product.type === 'ads') {
+    return (<Ads product={product} />)
+  }
+  else {
+    return (<ProductCard product={product} />)
+  }
 }
